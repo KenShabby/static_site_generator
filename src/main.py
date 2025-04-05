e8fe93b9dae497df8e65824d29429fa27b83f031
Brendan Coen <brendan@sonic.net>
2025-04-05 15:16:22 -0700

    Now recursively building directories

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import os
+import pathlib
 import shutil
 
 from block_splitters import *
 from markdown_to_html_node import *
 
-SOURCE_DIR = "static/"
+STATIC_DIR = "static/"
 CONTENT_DIR = "content/"
 TEMPLATE_DIR = "."
 DEST_DIR = "public/"
@@ -21,8 +22,8 @@ def copy_static_to_public():
     nuke_dir(DEST_DIR)
 
     # Copy over everything from static to public
-    shutil.copytree(SOURCE_DIR, DEST_DIR)
-    print(f"Ran copytree({SOURCE_DIR}, {DEST_DIR})")
+    shutil.copytree(STATIC_DIR, DEST_DIR)
+    print(f"Ran copytree({STATIC_DIR}, {DEST_DIR})")
 
 
 def nuke_dir(path):
@@ -43,42 +44,52 @@ def extract_title(markdown)-> str:
 
 
 def generate_page(from_path, template_path, dest_path):
+    # Less than elegant way of shoehorning our output
+    dest_path = dest_path.replace('.md', '.html')
+    dest_path = dest_path.replace('content/', '')
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-
     #Read the markdown file at from_path and store the contents in a variable.
     try:
         print(f"Attempting to open: {from_path}")
-        with open(f"{from_path}index.md", "r") as read_file:
+        with open(f"{from_path}", "r") as read_file:
             print(f"Succeeded.")
             # Use your markdown_to_html_node function and .to_html() method to convert
             # the markdown file to an HTML string.
             content = read_file.read()
             # Use the extract_title function to grab the title of the page.
             page_title = extract_title(content)
-            print(page_title)
             parent_node = markdown_to_html_node(content)
             html = parent_node.to_html()
             # Read the template file at template_path and store the contents in a variable.
-            with open(f"template.html", "r") as template_file:
+            with open(f"{template_path}", "r") as template_file:
                 template = template_file.read()
             # Replace the {{ Title }} and {{ Content }} placeholders in the template
-            # with thfkjje HTML and title you generated.
+            # with the HTML and title you generated.
             template = template.replace("{{ Title }}", page_title)
             article = template.replace("{{ Content }}", html)
             # Write the new full HTML page to a file at dest_path. Be sure to create any
             # necessary directories if they don't exist.
-            with open(f'{dest_path}index.html', 'w') as write_file:
+            print(f"Trying to make dir: {os.path.dirname(dest_path)}")
+            pathlib.Path(os.path.dirname(f'{dest_path}')).mkdir(parents=True, exist_ok=True)
+            print(f"Trying to touch file: {dest_path}")
+            pathlib.Path(f"{dest_path}").touch()
+            print(f"Touched!")
+            with open(f'{dest_path}', 'w') as write_file:
                 write_file.write(article)
     except FileNotFoundError:
-        print("File not found.")
+        print(f"File not found. {dest_path}")
     except PermissionError:
         print("Permission denied.")
 
 def main():
 
     copy_static_to_public()
-    generate_page(CONTENT_DIR, TEMPLATE_DIR, DEST_DIR)
-
+    for root, dirs, files in pathlib.Path(CONTENT_DIR).walk():
+            # generate_page(f"./{root}",
+            #              f".", 
+            #              f"public/")
+        for name in files:
+            generate_page(f"{root}/{name}", f"template.html", f"{DEST_DIR}{root}/{name}")
     return
 
 
